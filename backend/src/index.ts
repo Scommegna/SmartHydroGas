613bad2a46b9a13691d2d503cffa90569af84d8f
Lucas Scommegna <scommegnal@hotmail.com>
2025-01-18 18:21:57 -0300

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,28 +23,35 @@ const swaggerFilePath = path.resolve(__dirname, "./swagger/swagger.yaml");
 
 connectDB();
 
-app.use(session({
-  secret: "secretKey", 
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false, 
-    maxAge: 24 * 60 * 60 * 1000, 
-    httpOnly: true, 
-    sameSite: "lax", 
-  }
-}));
+app.use(
+  session({
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "lax",
+    },
+  })
+);
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
-  credentials: true,               
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
@@ -53,8 +60,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(sessionConfig);
 
-app.use("/", uploadRoutes);
-app.use("/", userRoutes);
+app.use("/api", uploadRoutes);
+app.use("/api", userRoutes);
 
 const swaggerDocument = YAML.load(swaggerFilePath);
 
